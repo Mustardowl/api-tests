072e11c3e3114eab0973d4c15983c4e676e02d7e
Arianne Dilzer <dil23001@byui.edu>
2023-11-21 14:12:59 -0500

    try again

--- test/helloworld.test.js
+++
@@ -1,7 +0,0 @@
-import sayHello from "../utils/helloworld.mjs";
-import assert from "assert";
-
-it ("Tests Returned Name", () => {
-    const hello = sayHello();
-    assert.equal(hello, "Arianne");
-});
